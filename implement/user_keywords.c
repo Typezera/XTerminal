792b3cc68004c6c8af26d40374dd7fa0da4989f6
Typezera <willianbardela2131@hotmail.com>
2026-05-15 22:39:11 -0300

    feat: tokenizando os comandos

--- implement/user_keywords.c
+++ implement/user_keywords.c
@@ -1,11 +1,19 @@
 #include "user_keywords.h"
 #include <stdio.h>
+#include <string.h>
 
 int commands_keyword_user(){
-   char buffer_commands_user[40];
+   char buffer_commands_user[100];
 
-   printf(">");
+   printf(">");// captura os comando do usuário
    fgets(buffer_commands_user, sizeof(buffer_commands_user), stdin);
 
-    
+   buffer_commands_user[strcspn(buffer_commands_user, "\n")] = '\0';
+
+   char *command = strtok(buffer_commands_user, " ");
+
+   printf("%s", buffer_commands_user);
+
+
+   printf("%c", *command);
 }
