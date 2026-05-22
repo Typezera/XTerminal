6890bfa0fdfa766fdbf205bfa217c7680b0c82e2
Typezera <willianbardela2131@hotmail.com>
2026-05-21 21:38:54 -0300

    conclusão do projeto

--- main.c
+++ main.c
@@ -3,25 +3,14 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-
 int main()
 {
-    // printf("Welcome to XTerminal, please, insert commands you want\n");
 
     while (1)
     {
-        printf(">");// captura os comando do usuário
-        commands_keyword_user();
+        printf(">");
+        commands_keyword_user(&argc, argv);
     }
-    // pid_t x = fork();
-
-    // if (x == 0)//processo filho
-    // {
-    //     printf("Olá eu sou o processo filho, meu PID é: %d\n, e o PID do meu pai é: %d\n", getpid(), getppid());
-    // }else if (x > 0)//processo pai
-    // {
-    //     printf("Olá eu sou o processo pai e o meu filho é %d\n", x);
-    // }
 
     return 0;
 }
